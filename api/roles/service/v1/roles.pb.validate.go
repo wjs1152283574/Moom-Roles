40d651a2c524084b395811f6c41f883e7ee62d0d
Casso <1152283574@qq.com>
2022-06-04 14:17:49 +0800

    make api

--- api/roles/service/v1/roles.pb.validate.go
+++ api/roles/service/v1/roles.pb.validate.go
@@ -4358,6 +4358,14 @@ func (m *PermissionListRequest) validate(all bool) error {
 
 	var errors []error
 
+	// no validation rules for Page
+
+	// no validation rules for Limit
+
+	// no validation rules for Name
+
+	// no validation rules for Code
+
 	if len(errors) > 0 {
 		return PermissionListRequestMultiError(errors)
 	}
@@ -4459,6 +4467,42 @@ func (m *PermissionListResponse) validate(all bool) error {
 	}
 
 	var errors []error
+
+	for idx, item := range m.GetList() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, PermissionListResponseValidationError{
+						field:  fmt.Sprintf("List[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, PermissionListResponseValidationError{
+						field:  fmt.Sprintf("List[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return PermissionListResponseValidationError{
+					field:  fmt.Sprintf("List[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	// no validation rules for Total
 
 	if len(errors) > 0 {
 		return PermissionListResponseMultiError(errors)
@@ -4765,6 +4809,12 @@ func (m *PermissionEditRequest) validate(all bool) error {
 	}
 
 	var errors []error
+
+	// no validation rules for Id
+
+	// no validation rules for Name
+
+	// no validation rules for Code
 
 	if len(errors) > 0 {
 		return PermissionEditRequestMultiError(errors)
@@ -6223,3 +6273,123 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = RoleListResponse_RoleListItemValidationError{}
+
+// Validate checks the field values on
+// PermissionListResponse_PermissionListItem with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *PermissionListResponse_PermissionListItem) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on
+// PermissionListResponse_PermissionListItem with the rules defined in the
+// proto definition for this message. If any rules are violated, the result is
+// a list of violation errors wrapped in
+// PermissionListResponse_PermissionListItemMultiError, or nil if none found.
+func (m *PermissionListResponse_PermissionListItem) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *PermissionListResponse_PermissionListItem) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Id
+
+	// no validation rules for Name
+
+	// no validation rules for Code
+
+	// no validation rules for Cid
+
+	// no validation rules for Cname
+
+	// no validation rules for Ctime
+
+	// no validation rules for Utime
+
+	if len(errors) > 0 {
+		return PermissionListResponse_PermissionListItemMultiError(errors)
+	}
+
+	return nil
+}
+
+// PermissionListResponse_PermissionListItemMultiError is an error wrapping
+// multiple validation errors returned by
+// PermissionListResponse_PermissionListItem.ValidateAll() if the designated
+// constraints aren't met.
+type PermissionListResponse_PermissionListItemMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m PermissionListResponse_PermissionListItemMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m PermissionListResponse_PermissionListItemMultiError) AllErrors() []error { return m }
+
+// PermissionListResponse_PermissionListItemValidationError is the validation
+// error returned by PermissionListResponse_PermissionListItem.Validate if the
+// designated constraints aren't met.
+type PermissionListResponse_PermissionListItemValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e PermissionListResponse_PermissionListItemValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e PermissionListResponse_PermissionListItemValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e PermissionListResponse_PermissionListItemValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e PermissionListResponse_PermissionListItemValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e PermissionListResponse_PermissionListItemValidationError) ErrorName() string {
+	return "PermissionListResponse_PermissionListItemValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e PermissionListResponse_PermissionListItemValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sPermissionListResponse_PermissionListItem.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = PermissionListResponse_PermissionListItemValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = PermissionListResponse_PermissionListItemValidationError{}
