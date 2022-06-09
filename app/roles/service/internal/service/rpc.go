05bd2be2a05cb2ad424f636467431be18b970706
“casso” <1152283574@qq.com>
2022-06-10 00:41:00 +0800

    add file

--- app/roles/service/internal/service/rpc.go
+++ app/roles/service/internal/service/rpc.go
@@ -3,13 +3,18 @@ package service
 import (
 	"context"
 
-	pb "github.com/it-moom/moom-roles/api/roles/service/v1"
+	v1 "github.com/it-moom/moom-roles/api/roles/service/v1"
+	"github.com/it-moom/moom-roles/pkg/errors"
 )
 
-func (r *RolesService) CheckRole(ctx context.Context, req *pb.CheckRoleRequest) (*pb.CheckRoleResponse, error) {
-	return &pb.CheckRoleResponse{}, nil
+func (r *RolesService) CheckRole(ctx context.Context, req *v1.CheckRoleRequest) (*v1.CheckRoleResponse, error) {
+	if req.Id <= 0 || req.Code == "" {
+		return &v1.CheckRoleResponse{}, errors.ErrSystemBusy
+	}
+
+	return &v1.CheckRoleResponse{}, nil
 }
 
-func (r *RolesService) CheckPermission(ctx context.Context, req *pb.CheckPermissionRequest) (*pb.CheckPermissionResponse, error) {
-	return &pb.CheckPermissionResponse{}, nil
+func (r *RolesService) CheckPermission(ctx context.Context, req *v1.CheckPermissionRequest) (*v1.CheckPermissionResponse, error) {
+	return &v1.CheckPermissionResponse{}, nil
 }
